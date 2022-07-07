df59b4ab32b038d6851f6704dd5f21af61b58cd6
Mirza Bakti <66278794+mirzabakti@users.noreply.github.com>
2022-07-07 18:14:24 +0700

    membuat fitur complete todo dan membatasi jumlah todos

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -6,26 +6,39 @@ import TodoForm from "../components/TodoForm";
 import Todos from "../components/Todos";
 
 const TodoList = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+    { text: "Learn React!", isCompleted: false },
+    { text: "Learn React Redux!", isCompleted: false }
+  ]);
 
   const [showAdd, setShowAdd] = useState(false);
 
   const addTodo = (value) => {
-    const addedTodo = [...todos, { text: value }];
+    if (todos.length < 9) {
+      const addedTodo = [...todos, { text: value, isCompleted: false }];
+
+      setTodos(addedTodo);
+    } else {
+      alert("Only 9 todos is allowed!");
+    }
+  };
+
+  const completeTodo = (index) => {
+    const addedTodo = [...todos];
+    addedTodo[index].isCompleted = !addedTodo[index].isCompleted;
 
     setTodos(addedTodo);
   };
-
   const showAddToggle = () => setShowAdd(!showAdd);
 
-  // console.log("todos", todos);
+  console.log("todos", todos);
   // console.log("showAdd", showAdd);
 
   return (
     <Paper>
       <Header showAddToggle={showAddToggle} showAdd={showAdd} />
       <TodoForm addTodo={addTodo} showAdd={showAdd} />
-      <Todos todos={todos} />
+      <Todos todos={todos} completeTodo={completeTodo} />
     </Paper>
   );
 };
